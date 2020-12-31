f02519e8441670dca6bc1cc98bb038959aac79ff
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2020-12-31 02:04:06 -0300

    Create desafio011.py

---
+++ Desafios/desafio011.py
@@ -0,0 +1,4 @@
+altura = float(input("Informe a altura da parede:(em metros)\n"))
+largura = float(input("Informe a largura da parede:(em metros\n"))
+area = altura*largura
+print('Você irá precisar de {} litros de tinta para pintar essa parede de {}m²!'.format(area/2,area))
