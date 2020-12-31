dbf9423ef0635053f70871deab32380f2de478d7
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2020-12-31 01:55:53 -0300

    Create desafio006.py

---
+++ Desafios/desafio006.py
@@ -0,0 +1,4 @@
+num = float(input("Digite um número:\n"))
+print("O dobro de {} é: {}".format(num,num*2))
+print("O triplo de {} é: {}".format(num,num*3))
+print("A raíz quadrada de {} é: {}".format(num,num**0.5))
