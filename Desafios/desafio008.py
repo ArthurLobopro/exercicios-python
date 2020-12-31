d6901a7b17a13519a93d2f049a29c536b0524081
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2020-12-31 01:59:07 -0300

    create desafio008.py

---
+++ Desafios/desafio008.py
@@ -0,0 +1,3 @@
+metros = float(input("Digite quantos metros deseja converter:\n"))
+print("{} tem {} centímetros".format(metros,metros*100))
+print("{} tem {} milímetros".format(metros,metros*1000))
