0defe93e2f30e5feb7659d4223c4babc220c4e8c
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2020-12-31 02:00:38 -0300

    Create desafio009.py

---
+++ Desafios/desafio009.py
@@ -0,0 +1,5 @@
+num = int(input("Digite um número:\n"))
+print("Tabuada de ",num)
+for i in range(0, 11) :
+    print("{} * {} = {}".format(num,i,num*1))
+#O exercício não deveria ser feito dessa forma, porém por ter um conhecimento básico de programação, resolvi pesquisar e utilizar estruturas de repetição. 
