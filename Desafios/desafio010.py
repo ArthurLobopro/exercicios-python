25579f5b041b6471eacae73c66b4b78531ceed5d
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2020-12-31 02:09:31 -0300

    formatação de moedas update

--- Desafios/desafio010.py
+++ Desafios/desafio010.py
@@ -1,2 +1,2 @@
 real = float(input("Digite quantos reais você tem:\nR$"))
-print("Com R${} reais você pode obter US${:.2f} ".format(real,real/3.27))
+print("Com R${:.2f} reais você pode obter US${:.2f} ".format(real,real/3.27))
--- Desafios/desafio012.py
+++ Desafios/desafio012.py
@@ -1,2 +1,2 @@
 preco = float(input("Digite o preço do produto:\n"))
-print("Com 5% de desconto este produto custará: R${}".format(preco-(preco*5/100)))
+print("Com 5% de desconto este produto custará: R${:.2f}".format(preco-(preco*5/100)))
