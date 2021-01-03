31c140f4a4a51b3b83c351e1528504c0cf4bfd41
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-01-03 03:40:46 -0300

    Desafio de tocar um mp3, porém esta função não funciona

---
+++ Desafios/desafio021/desafio021.py
@@ -0,0 +1,9 @@
+import pygame
+pygame.mixer.init()
+pygame.mixer.music.load('audio.mp3')
+pygame.mixer.music.play()
+pygame.event.wait()
+#Erro inesperado, arquivo imcompleto
+# print('Você está escutando: Elektronomia - Sky High pt. II [NCS Release]')
+# print('Music provided by NoCopyrightSounds.\nWatch: https://youtu.be/lG6HVrrXup8')
+# print('Free Download / Stream: http://ncs.io/SkyHigh2')
