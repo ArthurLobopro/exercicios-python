c754cff5c65309956d0d2080e1a5c7f4b3ef5981
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-01-04 17:26:57 -0300

    Update desafio024.py

--- Desafios/desafio024.py
+++ Desafios/desafio024.py
@@ -1,4 +1,4 @@
-cidade = input('Digite a cidade em que você nasceu:\n')
+cidade = input('Digite a cidade em que você nasceu:\n').strip()
 if cidade.lower().find('santo')==0:
     print('{} começa com "Santo"'.format(cidade))
 else:
