9abb6fb9001e0f3e6cb35e591c939b4fa231c762
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-01-12 18:30:46 -0300

    pequenas correções

--- Desafios/desafio032.py
+++ Desafios/desafio032.py
@@ -1,2 +1,2 @@
 ano = int(input('Digite um ano:\n'))
-print('{} é um ano bissexto'.format(ano) if ano%4==0 else '{} não é um ano bissexto'.format(ano))
+print('{} é um ano bissexto'.format(ano) if ano%4==0 and ano%100!=0 or ano%400==0 else '{} não é um ano bissexto'.format(ano))
--- Desafios/desafio035.py
+++ Desafios/desafio035.py
@@ -1,6 +1,6 @@
-a = int(input('Digite o tamanho da reta a:\n'))
-b = int(input('Digite o tamanho da reta b:\n'))
-c = int(input('Digite o tamanho da reta c:\n'))
+a = float(input('Digite o tamanho da reta a:\n'))
+b = float(input('Digite o tamanho da reta b:\n'))
+c = float(input('Digite o tamanho da reta c:\n'))
 if (a+b>c and b+c>a and c+a>b):
     print('Estas retas podem formar um triângulo.')
 else:
