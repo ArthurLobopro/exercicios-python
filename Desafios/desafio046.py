1992b15dde50f70c234b53a2ed21e4f1eee2fd6b
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-01-15 02:41:15 -0300

    Create desafio046.py

---
+++ Desafios/desafio046.py
@@ -0,0 +1,32 @@
+from modules.cores import creturn
+from random import randint
+
+def win():
+    print('Você {}'.format(creturn('VENCEU!!!',c='green')))
+def lose():
+    print('Você {}'.format(creturn('PERDEU!!!',c='red')))
+
+options = ['pedra','papel','tesoura']
+print('Opções:\n[ 0 ] Pedra\n[ 1 ] Papel\n[ 2 ] Tesoura')
+jogador = int(input("Qual sua jogada?  ")) 
+computador = options[randint(0,2)]
+if jogador >=0 and jogador<=2:
+    jogador = options[jogador]
+    print('Você escolheu {} e o computador {}'.format(jogador,computador))
+    if jogador == 'pedra':
+        if computador == 'tesoura':
+            win()
+        else: 
+            lose()
+    if jogador == 'papel':
+        if computador == 'pedra':
+            win()
+        else: 
+            lose()
+    if jogador == 'tesoura':
+        if computador == 'papel':
+            win()
+        else: 
+            lose()
+else:
+    print('Opção inválida,tente novamente!')
