e3b3c53d21297a804a043e5be00ce10cdf9d6bfa
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-01-15 02:44:53 -0300

    Update desafio046.py

--- Desafios/desafio046.py
+++ Desafios/desafio046.py
@@ -5,6 +5,8 @@ def win():
     print('Você {}'.format(creturn('VENCEU!!!',c='green')))
 def lose():
     print('Você {}'.format(creturn('PERDEU!!!',c='red')))
+def empate():
+    print('Deu {}'.format(creturn('EMPATE',c='yellow')))
 
 options = ['pedra','papel','tesoura']
 print('Opções:\n[ 0 ] Pedra\n[ 1 ] Papel\n[ 2 ] Tesoura')
@@ -13,20 +15,23 @@ computador = options[randint(0,2)]
 if jogador >=0 and jogador<=2:
     jogador = options[jogador]
     print('Você escolheu {} e o computador {}'.format(jogador,computador))
-    if jogador == 'pedra':
-        if computador == 'tesoura':
-            win()
-        else: 
-            lose()
-    if jogador == 'papel':
-        if computador == 'pedra':
-            win()
-        else: 
-            lose()
-    if jogador == 'tesoura':
-        if computador == 'papel':
-            win()
-        else: 
-            lose()
+    if jogador == computador:
+        empate()
+    else:
+        if jogador == 'pedra':
+            if computador == 'tesoura':
+                win()
+            else: 
+                lose()
+        if jogador == 'papel':
+            if computador == 'pedra':
+                win()
+            else: 
+                lose()
+        if jogador == 'tesoura':
+            if computador == 'papel':
+                win()
+            else: 
+                lose()
 else:
     print('Opção inválida,tente novamente!')
