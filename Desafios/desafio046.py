5b67152b8d39568f1dbb4c74f24c78b96b51779b
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-01-17 00:27:32 -0300

    Update desafio046.py

--- Desafios/desafio046.py
+++ Desafios/desafio046.py
@@ -1,5 +1,6 @@
 from time import sleep
-print(' '+'_'*20)
+from modules.cores import creturn
 for i in range(10,0,-1):
     print(i)
     sleep(1)
+print('{} {} {}'.format(creturn('BOMMM!',c='blue'),creturn('BOMM!',c='red'),creturn('BOMM!',c='yellow')))
