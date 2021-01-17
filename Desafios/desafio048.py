af12d1b02b24a11e3c7885c95b132f70846071ae
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-01-17 01:13:58 -0300

    Update desafio048.py

--- Desafios/desafio048.py
+++ Desafios/desafio048.py
@@ -1,4 +1,7 @@
 soma = 0
+cont = 0
 for i in range(0,500,3):
-    soma+=i
-print('A soma de todos os múltiplos de 3 entre 1 e 500 é{}'.format(soma))
+    if i%2!=0:
+        soma+=i
+        cont+=1
+print('A soma de todos os {} múltiplos de 3 ímpares entre 1 e 500 é {}'.format(cont,soma))
