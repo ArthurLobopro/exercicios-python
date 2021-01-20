e9083837ebc54177c04d5c944e62a9ccd3a8848c
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-01-20 12:57:15 -0300

    Update desafio053.py

--- Desafios/desafio053.py
+++ Desafios/desafio053.py
@@ -4,11 +4,10 @@ def replaceAll(string,old=' ',new=''):
         string=string.replace(old,new)
     return string
 
-frase = str(input('Digite uma frase:  ')).strip()
+frase = str(input('Digite uma frase:  ')).strip().lower()
 frase = replaceAll(frase)
 reverse = ''
 for i in range(len(frase)-1,-1,-1):
     reverse+=frase[i]
 print('O inverso de {} é {}.'.format(frase,reverse))
 print('Temos um palíndromo' if frase==reverse else 'Esta frase não é um palíndromo')
-
