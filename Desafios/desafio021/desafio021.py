d3cd670c7933065f91185eb7ed560782337806f9
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2022-07-04 18:57:33 -0300

    Desafio 21 concertado

--- Desafios/desafio021/desafio021.py
+++ Desafios/desafio021/desafio021.py
@@ -1,4 +1,8 @@
+from posixpath import dirname
 import pygame
+from os import path
+
+AUDIO_PATH = path.join(path.dirname(__file__), "audio.mp3")
 
 print('Você está escutando: Elektronomia - Sky High pt. II [NCS Release]')
 print('Music provided by NoCopyrightSounds.\nWatch: https://youtu.be/lG6HVrrXup8')
@@ -6,7 +10,8 @@ print('Free Download / Stream: http://ncs.io/SkyHigh2')
 
 pygame.init()
 pygame.mixer.init()
-pygame.mixer.music.load('audio.mp3')
+pygame.mixer.music.load(AUDIO_PATH)
 pygame.mixer.music.play()
 
-#Erro inesperado, arquivo imcompleto
+while True:
+    continue
