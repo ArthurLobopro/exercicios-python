3a5ce68eb8295660f387c577f4d5901057b0efab
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2024-02-21 13:25:16 -0300

    Desafio 98 concluído

---
+++ Desafios/desafio098/desafio098.py
@@ -0,0 +1,35 @@
+from re import sub
+
+
+def contador(FROM, TO, interval=1):
+    print(f"Contando de {FROM} até {TO} de {interval} em {interval}")
+
+    if FROM > TO and interval > 0:
+        interval *= -1
+
+    # range doesn't includes the last term, to we need an aditional
+    aditional = -1 if FROM > TO else 1
+
+    ARR = list(range(FROM, TO + aditional, interval))
+    print(sub(r"\[|\]", "", str(ARR)), "Fim!")
+
+
+def line():
+    print("-="*20)
+
+
+line()
+
+contador(1, 10)
+line()
+
+contador(10, 0, 2)
+line()
+
+print("Agora é sua vez de personalizar a contagem!")
+FROM = int(input("Início: "))
+TO = int(input("FIM: "))
+PASS = int(input("Passo: "))
+line()
+
+contador(FROM, TO, PASS)
