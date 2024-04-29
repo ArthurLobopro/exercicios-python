9b3d6391d3185dcc8362acb3282dab216f77c1f5
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2024-04-29 09:30:17 -0300

    Desafio 110

---
+++ Desafios/desafio110/desafio110.py
@@ -0,0 +1,5 @@
+from moeda import resumo
+
+value = float(input("Digite o valor do produto:  ").strip())
+
+resumo(value, 1.5, 8)
---
+++ Desafios/desafio110/moeda.py
@@ -0,0 +1,47 @@
+from os import get_terminal_size
+
+TERMINAL_SIZE = get_terminal_size().columns
+TABLE_SIZE = TERMINAL_SIZE//2
+
+
+def dobro(val, isFormated=False):
+    result = val * 2
+    return moeda(result) if isFormated else result
+
+
+def metade(val, isFormated=False):
+    result = val / 2
+    return moeda(result) if isFormated else result
+
+
+def aumentar(val, percentage, isFormated=False):
+    result = val + (val * percentage / 100)
+    return moeda(result) if isFormated else result
+
+
+def diminuir(val, percentage, isFormated=False):
+    result = val - (val * percentage / 100)
+    return moeda(result) if isFormated else result
+
+
+def moeda(val):
+    return f"R${val:.2f}"
+
+
+def line():
+    print("-"*TABLE_SIZE)
+
+
+def spaceBetween(left: str, right: str):
+    size = TABLE_SIZE - len(right + left)
+    print(f"{left}{(' ' * size)}{right}")
+
+
+def resumo(val, aumento, desconto):
+    line()
+    print("TABELA DE PAGAMENTO".center(TABLE_SIZE))
+    line()
+    spaceBetween("Preço Base:", moeda(val))
+    spaceBetween("Preço no Cartão:", aumentar(val, aumento, True))
+    spaceBetween("Preço à Vista:", diminuir(val, desconto, True))
+    line()
