ab1c20731e6e1642c47b2482ad94405b9d7974ed
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2024-04-29 17:36:41 -0300

    Desafio 111 concluído

---
+++ Desafios/desafio111/desafio111.py
@@ -0,0 +1,6 @@
+from utilidades.moeda import resumo
+
+
+value = float(input("Digite o valor do produto:  ").strip())
+
+resumo(value, 1.5, 8)
---
+++ Desafios/desafio111/utilidades/moeda/__init__.py
@@ -0,0 +1,3 @@
+from .moeda import moeda, aumentar, diminuir, dobro, resumo, metade
+
+__all__ = ["moeda", "aumentar", "diminuir", "dobro", "resumo", "metade"]
